7c86059eb50d9d2fc2c0698b2d8492e1dff3ef9a
Kadon <englekadondax@gmail.com>
2017-07-11 16:22:23 -0700

    Update for personal computer

--- OpenRAVETrainingVideo/TrainingVideo.py
+++ OpenRAVETrainingVideo/TrainingVideo.py
@@ -20,6 +20,7 @@ class TrainingVideo(object):
 		self.Hand = HandVis(self.vis)
 		self.Hand.loadHand()
 		self.Obj = ObjectGenericVis(self.vis)
+		self.GP = AddGroundPlane(self.vis)
 		self.demoObj = list()
 		self.frameCount = 0
 		self.start_offset = 0
@@ -29,49 +30,92 @@ class TrainingVideo(object):
 		self.vis.takeImage(fn, delay = False)
 		self.frameCount += 1
 
+	def fingerRecord(self, oldJA, newJA): # Records a frame for multiple join angles between the starting array (OldJA) amd the ending array (newJA)
+		try:
+			frame_rate = 20 # An arbitrary base line for the frames in each hand movement
+			frames = int(max(abs(newJA - oldJA)) * frame_rate)
+			print frames
+			Angles = []
+			for i in range(frames):
+				Angles.append(list())
+			for i in range(len(oldJA)):
+				current = np.linspace(oldJA[i], newJA[i], frames)
+				for k in range(frames):
+					Angles[k].append(current[k])
+			for i in Angles:
+				self.Hand.setJointAngles(np.array(i))
+				self.recordFrame()
+		except:
+			print "Invalid Joint Angle"
 
-	def handRecord(self, positions, dim):
-		if dim == 'z':
-			idx = 2
-		elif dim == 'x':
-			idx = 0
-		elif dim == 'y':
-			idx = 1
-		else:
-			print('Not a valid Dimension')
-			return
-		T = copy.deepcopy(self.T_start)
-		for i in positions:
-			T[idx,3] = i + self.T_start[idx,3]
-			self.Hand.obj.SetTransform(T)
-			self.recordFrame()
-			
+	def handRecord(self, x, y, z):
+		self.T_current = self.Hand.obj.GetTransform()
+		T = copy.deepcopy(self.T_current)
+		frames = 20
+		xyz  = [x, y, z]
+		for i in xyz:
+			if i == x:
+				X = np.linspace(self.T_current[0,3], x, frames)
+				for n in X:
+					T[0,3] = n
+					self.Hand.obj.SetTransform(T)
+					self.recordFrame()
+			elif i == y:
+				Y = np.linspace(self.T_current[1,3], y, frames)
+				for n in Y:
+					T[1,3] = n
+					self.Hand.obj.SetTransform(T)
+					self.recordFrame()
+			elif i == z:
+				Z = np.linspace(self.T_current[2,3], z, frames)
+				for n in Z:
+					T[2,3] = n
+					self.Hand.obj.SetTransform(T)
+					self.recordFrame()
+
+	# def handRecord(self, positions, dim):
+	# 	if dim == 'z':
+	# 		idx = 2
+	# 	elif dim == 'x':
+	# 		idx = 0
+	# 	elif dim == 'y':
+	# 		idx = 1
+	# 	else:
+	# 		print('Not a valid Dimension')
+	# 		return
+	# 	T = copy.deepcopy(self.T_start)
+	# 	for i in positions:
+	# 		T[idx,3] = i + self.T_start[idx,3]
+	# 		self.Hand.obj.SetTransform(T)
+	# 		self.recordFrame()
 
 	def stationaryRecord(self, frames):
 		for i in range(int(frames)):
 			self.recordFrame()
 
 	def createVideo(self, fn): # creates a video from images recorded in previous section
-		#there is a built in openrave function which would be much better, but doesn't work on all hardware
+		# there is a built in openrave function which would be much better, but doesn't work on all hardware
 		
 		# uses PIL and cv2. opencv is a pain to setup, so may not be worth the hassle
-		if os.path.splitext(fn)[1] != '.avi':
-			print("Did not save Video: Invalid File Name")
-			return
-		initImage = Image.open('Image0001.png')
-		height, width, layers = np.array(initImage).shape
-		fourcc = cv2.VideoWriter_fourcc(*'XVID')
-		video = cv2.VideoWriter(fn, fourcc, 24, (width, height))
-		Files = os.listdir(curdir)
-		for file in np.sort(Files):
-			if os.path.splitext(file)[1] == '.png':
-				image = Image.open(file)
-				video.write(cv2.cvtColor(numpy.array(image), cv2.COLOR_RGB2BGR))
-		video.release()
-
+		try:
+			if os.path.splitext(fn)[1] != '.avi':
+				print("Did not save Video: Invalid File Name")
+				return
+			initImage = Image.open('Image0001.png')
+			height, width, layers = np.array(initImage).shape
+			fourcc = cv2.VideoWriter_fourcc(*'XVID')
+			video = cv2.VideoWriter(fn, fourcc, 24, (width, height))
+			Files = os.listdir(curdir)
+			for file in np.sort(Files):
+				if os.path.splitext(file)[1] == '.png':
+					image = Image.open(file)
+					video.write(cv2.cvtColor(numpy.array(image), cv2.COLOR_RGB2BGR))
+			video.release()
+		except:
+			print 'Something went wrong. Maybe you didn\'t record any images'
 
 		# native linux method -- most likely to work
-		#subprocess.call(["avconv", "-f", "image2", "-i", "Image%04d.png", "-r", "5", "-s", "800x600", fn+".avi"])
+		# subprocess.call(["avconv", "-f", "image2", "-i", "Image%04d.png", "-r", "5", "-s", "800x600", fn+".avi"])
 
 	def removeImages(self): # remove image files that were created
 		Files = os.listdir(curdir)
@@ -87,6 +131,7 @@ class TrainingVideo(object):
 		self.Obj.loadObject('cube',36,18,3,None)  # TODO: Object with larger extent!
 		self.Obj.changeColor('greenI')
 		self.Hand.changeColor('blueI')
+		self.GP.createGroundPlane(0.175)
 		extent_offset = -3.0/100 # offset by thickness of object
 		palm_offset = -0.075 #offset so palm is at 0,0,0
 		clearance_offset = -1.0/100 # offset to have clearance between palm and object
@@ -95,38 +140,76 @@ class TrainingVideo(object):
 		rot = matrixFromAxisAngle([0,0, np.pi/2])
 		self.Hand.localRotation(rot) # rotate hand to put stationary finger on one side of object
 		self.T_start = self.Hand.obj.GetTransform() # get starting transform so everything can be done relative to this
-		JA = np.array([0, 0, 0.2, 0.8, 0.0, 0.2, 0.8, 0.0, 0.8, 0.0]) # position fingers such that hand can move in x, y, and z without hitting object
-		self.Hand.setJointAngles(JA)
+		oHand = np.array([0, 0, 0.0, 0.1, 0.1, 0.0, 0.1, 0.1, 0.1, 0.1]) # position fingers such that hand can move in x, y, and z without hitting object
+		cHand = np.array([0, 0, 0.0, 1.3, 0.4, 0.0, 1.3, 0.4, 1.3, 0.4]) # position fingers such that the hand will be closed around the object without hititng it
+		self.Hand.setJointAngles(oHand)
 		dist_range_min = -0.1
 		dist_range_max = 0.1
 		frame_rate = 20/0.1 # frames/cm
 		# TODO: **************
 		# fix camera angle so it is easier to see
-		self.vis.setCamera(60,3*np.pi/4,-np.pi/4-0.1)
-		# movement in z direction
-		d_max = 0.01
-		d_min = -0.1
-		frames = abs(d_max - d_min) * frame_rate
-		self.handRecord(np.linspace(d_max,d_min,frames), 'z')
-
-		# camera rotation between each scene?
-		# pdb.set_trace()
+		# self.vis.setCamera(60,3*np.pi/4,-np.pi/4-0.1)
+		self.vis.setCamera(60, -2.25, -0.75) # Numbers are completely arbitrary, gives a good view of object and hand.
+		
+		print self.T_start
+		pdb.set_trace()
 
 
-		# movement in x direction
-		d_max = 0.02
-		d_min = -0.03
-		frames = abs(d_max - d_min) * frame_rate
-		self.handRecord(np.linspace(d_max,d_min,frames), 'x')
-		self.stationaryRecord(10)
-		# movement in y direction
-		d_max = 0.2
-		d_min = -0.2
-		frames = abs(d_max - d_min) * frame_rate
-		self.handRecord(np.linspace(d_max,d_min,frames), 'y')
+		self.handRecord(.04, .05, -.03)
 
+		# # movement in z direction
+		# d_max = 0.00
+		# d_min = -0.1
+		# frames = abs(d_max - d_min) * frame_rate
+		# self.handRecord(np.linspace(d_max,d_min,frames), 'z')
+		# self.fingerRecord(oHand, cHand)
+		# self.stationaryRecord(5)
+		# self.fingerRecord(cHand, oHand)
+		# d_max = -0.1
+		# d_min = 0.0
+		# frames = abs(d_max - d_min) * frame_rate
+		# self.handRecord(np.linspace(d_max, d_min, frames), 'z')
 
+		# # movement in y direction
+		# d_max = 0.0
+		# d_min = 0.10
+		# frames = abs(d_max - d_min) * frame_rate
+		# self.handRecord(np.linspace(d_max,d_min,frames), 'y')
+		# self.fingerRecord(oHand, cHand)
+		# self.stationaryRecord(5)
+		# self.fingerRecord(cHand, oHand)
 
+		# d_max = 0.10
+		# d_min = -0.10
+		# frames = abs(d_max - d_min) * frame_rate
+		# self.handRecord(np.linspace(d_max, d_min, frames), 'y')
+		# self.fingerRecord(oHand, cHand)
+		# self.stationaryRecord(5)
+		# self.fingerRecord(cHand, oHand)
+
+		# d_max = -0.10
+		# d_min = 0.0
+		# frames = abs(d_max - d_min) * frame_rate
+		# self.handRecord(np.linspace(d_max, d_min, frames), 'y')
+		
+		# # movement in x direction
+		# d_max = 0.0
+		# d_min = -0.04
+		# frames = abs(d_max - d_min) * frame_rate
+		# self.handRecord(np.linspace(d_max,d_min,frames), 'x')
+		# self.stationaryRecord(10)
+
+		# d_max = -0.04
+		# d_min = 0.04
+		# frame = abs(d_max - d_min) * frame_rate
+		# self.handRecord(np.linspace(d_max, d_min, frames), 'x')
+		# self.stationaryRecord(10)
+
+		# d_max = 0.04
+		# d_min = 0.0
+		# frame = abs(d_max - d_min) * frame_rate
+		# self.handRecord(np.linspace(d_max, d_min, frames), 'x')
+		# self.stationaryRecord(10)
 
 
 
@@ -136,6 +219,6 @@ if __name__ == '__main__':
 	TV = TrainingVideo()
 	TV.Video1()
 	TV.createVideo('Video1.avi')
-	TV.VLCPlay('Video1.avi')
 	TV.removeImages()
+	TV.VLCPlay('Video1.avi')
 	pdb.set_trace()
